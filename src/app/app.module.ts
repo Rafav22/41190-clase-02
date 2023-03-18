6feda5a39779f53edb2c6e487ea8e15d6ff54086
Rafav22 <rafav22@gmail.com>
2023-03-18 15:29:51 -0600

    CAMBIOS, Se agrego la libreria de Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent,
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule
